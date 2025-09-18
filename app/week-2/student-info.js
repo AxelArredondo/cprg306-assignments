f8bdec4915b1b66fd127040f41a379b47f4e3b74
AxelArredondo <axel.arredondotapia@edu.sait.ca>
2025-09-18 09:38:45 -0600

    added week 2 lab

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+export default function StudentInfo() {
+    return (
+        <main>
+            <h2>Axel Arredondo</h2>
+            <Link href="https://github.com/AxelArredondo/cprg306-assignments">Github Repo Link</Link>
+        </main>
+    ); 
+}
